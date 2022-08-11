942cebb98130182b434e54f339a8b71a50331eb1
CGandGameEngineLearner <1353321741@qq.com>
2022-08-11 17:35:27 +0800

    3

--- Engine/Source/Runtime/Core/Meta/Reflection/reflection.h
+++ Engine/Source/Runtime/Core/Meta/Reflection/reflection.h
@@ -12,4 +12,17 @@ namespace Movan
 #define META(...) __attribute__((annotate(#__VA_ARGS__)))
 #define CLASS(class_name, ...) class __attribute__((annotate(#__VA_ARGS__))) class_name
 #define STRUCT(struct_name, ...) struct __attribute__((annotate(#__VA_ARGS__))) struct_name
+	//#define CLASS(class_name,...) class __attribute__((annotate(#__VA_ARGS__))) class_name:public Reflection::object
+#else
+	//变长参数
+#define META(...)
+#define CLASS(class_name,...)class class_name;
+#define STRUCT(struct_name,...)struct struct_name;
+	//#define CLASS(class_name,...) class class_name:public Reflection::object
+#endif // __REFLECTION_PARSER__
+
+#define REFLECTION_BODY(class_name) \
+    friend class Reflection::TypeFieldReflectionOparator::Type##class_name##Operator; \
+    friend class PSerializer;
+	// public: virtual std::string getTypeName() override {return #class_name;}
 }
