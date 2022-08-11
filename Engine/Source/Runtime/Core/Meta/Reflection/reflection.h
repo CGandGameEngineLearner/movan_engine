fd2d2c778f41ee62394048a29b6e5dab2dd328e0
CGandGameEngineLearner <1353321741@qq.com>
2022-08-11 22:50:57 +0800

    2

--- Engine/Source/Runtime/Core/Meta/Reflection/reflection.h
+++ Engine/Source/Runtime/Core/Meta/Reflection/reflection.h
@@ -25,4 +25,61 @@ namespace Movan
     friend class Reflection::TypeFieldReflectionOparator::Type##class_name##Operator; \
     friend class PSerializer;
 	// public: virtual std::string getTypeName() override {return #class_name;}
+
+
+
+
+
+
+	namespace Reflection
+	{
+		class TypeMetaRegisterinterface//元数据注册接口
+		{
+		public:
+			static void registerToClassMap(const char* name, ClassFunctionTuple* value);
+			static void registerToFieldMap(const char* name, FieldFunctionTuple* value);
+			static void registerToArrayMap(const char* name, ArrayFunctionTuple* value);
+
+			static void unregisterAll();
+		};
+        class TypeMeta//元数据类型
+        {
+            friend class FieldAccessor;
+            friend class ArrayAccessor;
+            friend class TypeMetaRegisterinterface;
+
+        public:
+            TypeMeta();
+
+            // static void Register();
+
+            static TypeMeta newMetaFromName(std::string type_name);
+
+            static bool               newArrayAccessorFromName(std::string array_type_name, ArrayAccessor& accessor);
+            static ReflectionInstance newFromNameAndPJson(std::string type_name, const PJson& json_context);
+            static PJson              writeByName(std::string type_name, void* instance);
+
+            std::string getTypeName();
+
+            int getFieldsList(FieldAccessor*& out_list);
+
+            int getBaseClassReflectionInstanceList(ReflectionInstance*& out_list, void* instance);
+
+            FieldAccessor getFieldByName(const char* name);
+
+            bool isValid() { return m_is_valid; }
+
+            TypeMeta& operator=(const TypeMeta& dest);
+
+        private:
+            TypeMeta(std::string type_name);
+
+        private:
+            std::vector<FieldAccessor, std::allocator<FieldAccessor>> m_fields;
+
+            std::string m_type_name;
+
+            bool m_is_valid;
+        };
+	}
 }
