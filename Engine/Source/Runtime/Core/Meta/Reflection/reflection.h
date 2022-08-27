de8737229cdeb6a0115636d1ff9160ad625df80f
CGandGameEngineLearner <1353321741@qq.com>
2022-08-27 21:48:57 +0800

    111

--- Engine/Source/Runtime/Core/Meta/Reflection/reflection.h
+++ Engine/Source/Runtime/Core/Meta/Reflection/reflection.h
@@ -8,7 +8,7 @@
 
 namespace Movan
 {
-#if defined(__REFLECTION_PARSER__)//如果预设了反射解析器
+#if defined(__REFLECTION_PARSER__)//如果预设了反射解析器 
 #define META(...) __attribute__((annotate(#__VA_ARGS__)))
 #define CLASS(class_name, ...) class __attribute__((annotate(#__VA_ARGS__))) class_name
 #define STRUCT(struct_name, ...) struct __attribute__((annotate(#__VA_ARGS__))) struct_name
