c4d8868553f6d447e44b3d607f3ca84fd8505adc
CGandGameEngineLearner <1353321741@qq.com>
2022-09-10 22:58:14 +0800

    8468

---
+++ Engine/Source/MetaParser/Parser/Meta/meta_data_config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace NativeProperty//NativeProperty是为了能设置反射系统的属性设置的
+{
+    const auto All = "All";
+
+    const auto Fields = "Fields";
+
+    const auto Enable = "Enable";
+    const auto Disable = "Disable";
+
+    const auto WhiteListFields = "WhiteListFields";//白名单模式
+
+} // namespace NativeProperty
--- Engine/Source/MetaParser/Parser/Meta/meta_utils.h
+++ Engine/Source/MetaParser/Parser/Meta/meta_utils.h
@@ -1,17 +1,1 @@
 #pragma once
-namespace Utils
-{
-    template<typename A, typename B>
-    bool rangeEqual(A startA, A endA, B startB, B endB)//检查是否相同
-    {
-        while (startA != endA && startB != endB)
-        {
-            if (*startA != *startB)
-                return false;
-
-            ++startA;
-            ++startB;
-        }
-
-        return (startA == endA) && (startB == endB);
-    }
