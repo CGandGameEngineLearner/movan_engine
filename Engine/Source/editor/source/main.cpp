145ed3952564d07dcbccd2e3bcffe08dd43f264c
CGandGameEngineLearner <1353321741@qq.com>
2024-07-14 17:39:31 +0800

    Refactor

--- Engine/Source/editor/source/main.cpp
+++ Engine/Source/editor/source/main.cpp
@@ -1,7 +1,8 @@
+#pragma once
 #include <filesystem>
-#include <string>
 
-#include "source/runtime/engine.cppm"
+
+import MovanEngine;
 
 
 // https://gcc.gnu.org/onlinedocs/cpp/Stringizing.html
@@ -11,13 +12,13 @@
 int main(int argc, char** argv)
 {
     std::filesystem::path executable_path(argv[0]);
+
     std::filesystem::path config_file_path = executable_path.parent_path() / "MovanEditor.ini";
 
-    Movan::MovanEngine* engine = new Movan::MovanEngine();//初始化引擎
+    std::unique_ptr<Movan::MovanEngine> engine = std::make_unique<Movan::MovanEngine>();//初始化引擎
 
     engine->startEngine(config_file_path.generic_string());
     engine->initialize();
 
-    Movan::MovanEditor* editor = new Movan::MovanEditor();
 
 }
